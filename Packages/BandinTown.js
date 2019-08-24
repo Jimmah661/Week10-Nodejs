0f84c118fcbd905c604270908a48400fa4a92200
James Rudd <james.rudd45@gmail.com>
2019-08-24 19:18:34 +1000

    Spotify call functional

--- Packages/BandinTown.js
+++ Packages/BandinTown.js
@@ -1,12 +1,28 @@
-var bandCall = function(bandName) {
+require("dotenv").config();
+var axios = require("axios");
+var mapquestKey = process.env.MAPQUEST_KEY;
+
+var bandCall = function (bandName) {
   axios({
     method: "get",
     url:
-      "https://rest.bandsintown.com/artists/Electric%20Six/events?app_id=codingbootcamp&date=upcoming"
-  }).then(function(response) {
+      "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp&date=upcoming"
+  }).then(function (response) {
     var array = response.data;
-    array.forEach(function(el) {
-      console.log("venue Name: " + el.venue.name + ", Date: " + el.datetime);
+    console.log(array);
+    array.forEach(function (el) {
+      var dateArray = el.datetime.split("T");
+      var location = [el.latitude, el.longitude];
+      // axios.get("http://www.mapquestapi.com/geocoding/v1/reverse?key=" + mapquestKey + "&location=" + location[0] + "," + location[1])
+      //   .then(function (res) { console.log(res.results.locations.street) })
+      console.log(
+        "venue Name: " +
+        el.venue.name +
+        ",\nDate: " +
+        dateArray[0] +
+        ", Time: " +
+        dateArray[1].substring(0, dateArray[1].length - 3)
+      );
     });
   });
 };
--- liri.js
+++ liri.js
@@ -1,41 +1,43 @@
 require("dotenv").config();
 var keys = require("./key.js");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+// External module calls
 var bandCall = require("./Packages/BandinTown.js");
-var axios = require("axios");
-var fs = require("fs");
+var omdbCall = require("./Packages/omdbCall")
 var operator = process.argv[2];
-var workIt = process.argv[3];
+var workIt = process.argv.slice(3).join(" ");
 
 // var spotify = new Spotify(keys.spotify);
-
-// var bandCall = function(bandName) {
-//   axios({
-//     method: "get",
-//     url:
-//       "https://rest.bandsintown.com/artists/Electric%20Six/events?app_id=codingbootcamp&date=upcoming"
-//   }).then(function(response) {
-//     var array = response.data;
-//     array.forEach(function(el) {
-//       console.log("venue Name: " + el.venue.name + ", Date: " + el.datetime);
-//     });
-//   });
-// };
 
 switch (operator) {
   // Connect to Bands in Town and output information based on the README file (Line 142)
   case "concert-this":
-    // AXIOS connect to the "Bands in Town Artists events API"
-    // Render Name of venue, Venue Location and event date
 
-    bandCall(workIt);
+
+    bandCall.bandCall(workIt);
     break;
 
   // Connect to Spotify and output information based on the README file (Line 152)
   case "spotify-this-song":
+    spotify.search({ type: 'track', query: workIt }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      var response = data.tracks.items[0]
+      var string = ["Artist Name: " + response.artists[0].name,
+      "Song Name: " + response.name,
+      "Link: " + response.album.external_urls.spotify,
+      "Album Title: " + response.album.name
+      ].join('\n\n')
+      console.log(string);
+    });
     break;
 
   // Connect to OMDB and output information based on the README file (Line 178)
   case "movie-this":
+    omdbCall.call(workIt)
     break;
 
   // Run the Liribot using commands stored in the random.txt file
